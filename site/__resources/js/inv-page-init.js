6d20b3f2f6ce1939548c4499272b31db541d5947
BravestCheetah <bravestcheetah@gmail.com>
2025-08-05 16:37:19 +0200

    added more logging to on reload inventory load

--- site/__resources/js/inv-page-init.js
+++ site/__resources/js/inv-page-init.js
@@ -1,5 +1,8 @@
+console.log("loading inventory data...")
+
 items = load_stored_data();
 
 items.forEach(item => {
+    console.log(`  - loading item "${item}"...`)
     addItem(item.split(".")[0], item.split(".")[1])
 });
