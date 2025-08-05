8e8d0ee71f8e3e2c9ca0ef7b126e5a1b093dccee
BravestCheetah <bravestcheetah@gmail.com>
2025-08-05 16:25:29 +0200

    updated check if wonsded function accordingly

--- site/__resources/js/inv.js
+++ site/__resources/js/inv.js
@@ -19,44 +19,8 @@ function addItem(category, item) {
 
 
 function check_if_owned(category, item) {
-    const container = inv_data.sections[category];
-    const divs = container.querySelectorAll("div");
-
-    for (const item_div of divs) {
-        const item_name = item_div.querySelector("p").textContent;
-        if (inv_data.items[category][item].name == item_name) {
-            return true;
-        }
-
-    };
-
-    return false;
-}
-
-
-function get_owned() {
-
-    let owned_items = [];
-
-    Object.keys(inv_data.items).forEach(category => {
-
-        const container = inv_data.sections[category];
-        const divs = container.querySelectorAll("div");
-
-        for (const item_div of divs) {
-            const item_name = item_div.querySelector("p").textContent;
-            
-            for (const [id, data] of Object.entries(inv_data.items[category])) {
-                if (data.name === item_name) {
-                    var item_id = `${category}.${id}`
-                    break;
-                }
-            }
-
-        owned_items.push(item_id)
-        };
-    });
-    return owned_items
+    const owned = loadOwnedItems();
+    return owned.includes(`${category}.${item}`);
 }
 
 
