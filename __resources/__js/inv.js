881f8f19e268bba39298f1f5f4b7c5428ab49a36
BravestCheetah <bravestcheetah@gmail.com>
2025-08-05 13:03:37 +0200

    added inv constant

--- __resources/__js/inv.js
+++ __resources/__js/inv.js
@@ -1,6 +1,32 @@
+// I am so sorry for this horrible code, its my first web dev project :sob:
+// oh yeah i like whitespace in variables / objects, so you know, thats why there are so many linebreaks
+
 const inv = {
+
     sections: {
-        "os":document.getElementsByClassName("inv-os"),
-        "lang":document.getElementsByClassName("inv-lang")
+        "os":document.getElementById("inv-os"),
+        "lang":document.getElementById("inv-lang"),
+    },
+
+
+
+    items: {
+
+        os: {
+            arch: {
+                rarity: "rare",
+                name: "Arch Linux",
+                image: "arch-linux.png"
+            }
+        },
+
+        lang: {
+            python: {
+                rarity: "rare",
+                name: "Python",
+                image: "python.png"
+            }
+        }
     }
+
 }
