d65482c746d20eac4bf05d60cab10fe2e666903b
BravestCheetah <bravestcheetah@gmail.com>
2025-08-05 19:14:59 +0200

    updated programming collection

--- site/__resources/js/roll-data.js
+++ site/__resources/js/roll-data.js
@@ -7,14 +7,21 @@ const roll_data = {
             name: "programming Languages",
 
             items: {
-                "lang.javascript": .22,
-                "lang.cs": .22,
-                "lang.cpp": .22,
+                "lang.javascript": 0.1239669421,
+                "lang.cs": 0.1239669421,
+                "lang.cpp": 0.1239669421,
+                "lang.php": 0.1239669421,
+                "lang.go": 0.1239669421,
 
-                "lang.python": .15,
-                "lang.clang": .15,
+                "lang.python": 0.0826446281,
+                "lang.clang": 0.0826446281,
+                "lang.rust": 0.0826446281,
+                "lang.java": 0.0826446281,
                 
-                "lang.holyc": .4,
+                "lang.holyc": 0.02066115702,
+                "lang.lol": 0.02066115702,
+
+                "lang.asm": 0.00826446281,
             }
         }
 
