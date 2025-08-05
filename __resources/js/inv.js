2b0df6720341a18028539acdd0ee9cc902195215
BravestCheetah <bravestcheetah@gmail.com>
2025-08-05 13:39:17 +0200

    moved inv data

--- __resources/js/inv.js
+++ __resources/js/inv.js
@@ -1,37 +1,5 @@
 // I am so sorry for this horrible code, its my first web dev project :sob:
 // oh yeah i like whitespace in variables / objects, so you know, thats why there are so many linebreaks
-// oh yeah also, yes the data and functions are in the same file, deal with it
-
-const inv = {
-
-    sections: {
-        "os": document.querySelector("#inv-os .inv-item-section"),
-        "lang": document.querySelector("#inv-lang .inv-item-section"),
-    },
-
-
-
-    items: {
-
-        os: {
-            arch: {
-                rarity: "rare",
-                name: "Arch Linux",
-                image: "arch-linux.png"
-            }
-        },
-
-        lang: {
-            python: {
-                rarity: "rare",
-                name: "Python",
-                image: "python.png"
-            }
-        }
-    }
-
-}
-
 
 
 function addItem(category, item) {
