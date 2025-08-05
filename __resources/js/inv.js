adf471bedd7ec34a8d953c622fb3f3a703bbdeb0
BravestCheetah <bravestcheetah@gmail.com>
2025-08-05 13:17:55 +0200

    completing additem func

--- __resources/js/inv.js
+++ __resources/js/inv.js
@@ -37,12 +37,14 @@ function addItem(category, item) {
     const container = inv.sections[category];
 
     item_container = document.createElement("div");
-    item_container.class = "inv-item";
+    item_container.className = "inv-item";
     container.appendChild(item_container);
 
     item_img = document.createElement("img");
     item_img.src = `../__resources/images/rolls/${category}/${inv.items[category][item].image}`;
     item_container.appendChild(item_img);
 
-    item_text = 
+    item_text = document.createElement("p");
+    item_text.textContent = inv.items[category][item].name;
+    item_container.appendChild(item_text);
 }
